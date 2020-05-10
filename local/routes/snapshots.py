059d2adc915603630dbdc72829b20a1c776e3c08
Eric <eric@pacefactory.com>
2020-05-10 10:53:23 -0400

    Created separate routing modules for snapshots & backgrounds in anticipation of implementing different routing functionality in the near future

---
+++ local/routes/snapshots.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  9 14:36:17 2020
+
+@author: eo
+"""
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+#%% Add local path
+
+import os
+import sys
+
+def find_path_to_local(target_folder = "local"):
+    
+    # Skip path finding if we successfully import the dummy file
+    try:
+        from local.dummy import dummy_func; dummy_func(); return
+    except ImportError:
+        print("", "Couldn't find local directory!", "Searching for path...", sep="\n")
+    
+    # Figure out where this file is located so we can work backwards to find the target folder
+    file_directory = os.path.dirname(os.path.abspath(__file__))
+    path_check = []
+    
+    # Check parent directories to see if we hit the main project directory containing the target folder
+    prev_working_path = working_path = file_directory
+    while True:
+        
+        # If we find the target folder in the given directory, add it to the python path (if it's not already there)
+        if target_folder in os.listdir(working_path):
+            if working_path not in sys.path:
+                tilde_swarm = "~"*(4 + len(working_path))
+                print("\n{}\nPython path updated:\n  {}\n{}".format(tilde_swarm, working_path, tilde_swarm))
+                sys.path.append(working_path)
+            break
+        
+        # Stop if we hit the filesystem root directory (parent directory isn't changing)
+        prev_working_path, working_path = working_path, os.path.dirname(working_path)
+        path_check.append(prev_working_path)
+        if prev_working_path == working_path:
+            print("\nTried paths:", *path_check, "", sep="\n  ")
+            raise ImportError("Can't find '{}' directory!".format(target_folder))
+            
+find_path_to_local()
+
+# ---------------------------------------------------------------------------------------------------------------------
+#%% Imports
+
+from local.lib.mongo_helpers import connect_to_mongo
+from local.lib.timekeeper_utils import time_to_epoch_ms
+
+from starlette.responses import UJSONResponse
+from starlette.routing import Route
+
+from pymongo import ASCENDING
+
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+#%% Create logging routes
+
+# .....................................................................................................................
+
+# .....................................................................................................................
+# .....................................................................................................................
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+#%% Define call functions
+
+# .....................................................................................................................
+
+def get_snapshot_collection(camera_select):
+    return mclient[camera_select]["snapshots"]
+
+# .....................................................................................................................
+
+def build_snapshot_routes():
+    '''
+    # Bundle all camera info routes
+    log_url = lambda log_route: "".join(["/{camera_select:str}/logs", log_route])
+    logging_routes = \
+    [
+     Route(log_url("/get-all-log-types"), logs_get_all_types),
+     Route(log_url("/{log_type:str}/since/{target_time}"), logs_since_target_time),
+     Route(log_url("/{log_type:str}/by-time-range/{start_time}/{end_time}"), logs_by_time_range),
+     Route(log_url("/{log_type:str}/count/by-time-range/{start_time}/{end_time}"), logs_count_by_time_range)
+    ]
+    '''
+    snapshot_routes = []
+    return snapshot_routes
+
+# .....................................................................................................................
+# .....................................................................................................................
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+#%% Global setup
+
+# Connection to mongoDB
+mclient = connect_to_mongo()
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+#%% Demo
+
+if __name__ == "__main__":
+    pass
+
+# ---------------------------------------------------------------------------------------------------------------------
+#%% Scrap
+
+
