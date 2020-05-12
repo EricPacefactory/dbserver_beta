93d9ab63fa59cd54304a560612d0b2259de09217
Eric <eric@pacefactory.com>
2020-05-12 16:16:55 -0400

    Reworked posting routes so that each collection now has a dedicated function handling it. This also allows for auto-object-indexing (on camera info posts!)

--- local/routes/posting.py
+++ local/routes/posting.py
@@ -53,99 +53,144 @@ from local.lib.mongo_helpers import connect_to_mongo, post_many_to_mongo
 from local.lib.response_helpers import post_success_response, not_allowed_response, bad_request_response
 from local.lib.image_pathing import build_base_image_pathing, build_image_pathing
 
+from local.routes.objects import set_object_indexing, check_object_indexing, get_object_collection
+
 from starlette.routing import Route
 
 
+# ---------------------------------------------------------------------------------------------------------------------
+#%% Create generic posting functions
+
+# .....................................................................................................................
+
+async def post_metadata_by_collection(request, collection_name):
+    
+    # Get data for posting
+    camera_select = request.path_params["camera_select"]
+    post_data_json = await request.json()
+    
+    # Send metadata to mongo
+    post_success, mongo_response = post_many_to_mongo(MCLIENT, camera_select, collection_name, post_data_json)
+    
+    # Return an error response if there was a problem posting
+    # Hard-coded: assuming the issue is with duplicate entries
+    if not post_success:
+        additional_response_dict = {"mongo_response": mongo_response}
+        error_message = "Error posting {} metadata. Entries likely exist already!".format(collection_name)
+        return not_allowed_response(error_message, additional_response_dict)
+    
+    return post_success_response()
+
+# .....................................................................................................................
+
+async def post_image_data_by_collection(request, collection_name):
+    
+    # Get information from route url
+    camera_select = request.path_params["camera_select"]
+    image_epoch_ms = request.path_params["epoch_ms"]
+    
+    # Generate the image file pathing, so we can first make sure the image doesn't already exist
+    image_save_path = build_image_pathing(IMAGE_FOLDER, camera_select, collection_name, image_epoch_ms,
+                                          create_folder_if_missing = True)
+    
+    # Return error if the image file has already been stored
+    if os.path.exists(image_save_path):
+        error_message = "Can't upload, image already exists ({})".format(image_epoch_ms)
+        return not_allowed_response(error_message)
+    
+    # Get the image data from the post body
+    image_data = await request.body()
+    if not image_data:
+        error_message = "No {} image data in body ({})".format(collection_name, image_epoch_ms)
+        return bad_request_response(error_message)
+    
+    # Save the data to the filesystem (not mongodb!)
+    with open(image_save_path, "wb") as out_file:
+        out_file.write(image_data)
+    
+    return post_success_response(image_epoch_ms)
+
+# .....................................................................................................................
+# .....................................................................................................................
+
 
 # ---------------------------------------------------------------------------------------------------------------------
-#%% Create data posting routes
+#%% Define metadata routes
 
 # .....................................................................................................................
 
-def post_metadata(collection_name):
+async def post_camerainfo(request):
     
-    '''
-    Function used to create functions used for handling metadata POSTing
-    Inputs:
-        collection_name: (String) --> Specify the collection of metadata to handle. 
-                                      Should be something like "snapshots", "objects", "camerainfo" etc.
+    # For clarity
+    collection_name = "camerainfo"
+    camera_select = request.path_params["camera_select"]
     
-    Outputs:
-        post_metadata_by_collection (function)
-        
-        (The output should be used to create a Route object)
-        (Ex: Route("/url/to/post/metadata", post_metadata_by_collection)
-    '''
+    # Use standard metadata posting
+    post_response = await post_metadata_by_collection(request, collection_name)
     
-    async def post_metadata_by_collection(request):
-        
-        # Convert post data to python data
-        post_data = await request.json()
-        
-        # Get the camera selection from the url path, and send off the data!
-        camera_select = request.path_params["camera_select"]
-        post_success, mongo_response = post_many_to_mongo(MCLIENT, camera_select, collection_name, post_data)
-        
-        # Return an error response if there was a problem posting 
-        # Hard-coded: assuming the issue is with duplicate entries
-        if not post_success:
-            additional_response_dict = {"mongo_response": mongo_response}
-            error_message = "Error posting metadata. Entries likely exist already!"
-            return not_allowed_response(error_message, additional_response_dict)
-        
-        return post_success_response()
+    # Try to set object indexing every time new camera info is posted
+    # (which should be infrequent, but indicates the camera has reset)
+    collection_ref = get_object_collection(camera_select)
+    object_indexes_are_set = check_object_indexing(collection_ref)
+    if not object_indexes_are_set:
+        set_object_indexing(collection_ref)
+        #print("  --> Object indexing set for {}".format(camera_select))
     
-    return post_metadata_by_collection
+    return post_response
 
 # .....................................................................................................................
 
-def post_image(image_category):
+async def post_background_metadata(request):
     
-    ''' 
-    Function used to create functions used for handling image POSTing 
-    Inputs:
-        image_category: (String) --> Specify the category of image to handle. 
-                                     Likely going to be either "snapshots" or "backgrounds"
+    # For clarity
+    collection_name = "backgrounds"
     
-    Outputs:
-        post_image_by_category (function)
-        
-        (The output should be used to create a Route object)
-        (Ex: Route("/url/to/post/image", post_image_by_category)
-    '''
+    return await post_metadata_by_collection(request, collection_name)
 
-    async def post_image_by_category(request):
-        
-        # Get information from route url
-        camera_select = request.path_params["camera_select"]
-        image_epoch_ms = request.path_params["epoch_ms"]
-        
-        # Generate the image file pathing, so we can first make sure the image doesn't already exist
-        image_save_path = build_image_pathing(IMAGE_FOLDER, camera_select, image_category, image_epoch_ms,
-                                              create_folder_if_missing = True)
-        
-        # Return error if the image file has already been stored
-        if os.path.exists(image_save_path):
-            error_message = "Can't upload, image already exists ({})".format(image_epoch_ms)
-            return not_allowed_response(error_message)
-        
-        # Get the image data from the post body
-        image_data = await request.body()
-        if not image_data:
-            error_message = "No {} image data in body ({})".format(image_category, image_epoch_ms)
-            return bad_request_response(error_message)
-        
-        # Save the data to the filesystem (not mongodb!)
-        with open(image_save_path, "wb") as out_file:
-            out_file.write(image_data)
-        
-        return post_success_response(image_epoch_ms)
+# .....................................................................................................................
+
+async def post_object_data(request):
     
-    return post_image_by_category
+    # For clarity
+    collection_name = "objects"
+    
+    return await post_metadata_by_collection(request, collection_name)
+
+# .....................................................................................................................
+
+async def post_snapshot_metadata(request):
+    
+    # For clarity
+    collection_name = "snapshots"
+    
+    return await post_metadata_by_collection(request, collection_name)
 
 # .....................................................................................................................
 # .....................................................................................................................
 
+# ---------------------------------------------------------------------------------------------------------------------
+#%% Define image routes
+
+# .....................................................................................................................
+
+async def post_background_image_data(request):
+    
+    # For clarity
+    collection_name = "backgrounds"
+    
+    return await post_image_data_by_collection(request, collection_name)
+
+# .....................................................................................................................
+
+async def post_snapshot_image_data(request):
+    
+    # For clarity
+    collection_name = "snapshots"
+    
+    return await post_image_data_by_collection(request, collection_name)
+
+# .....................................................................................................................
+# .....................................................................................................................
 
 # ---------------------------------------------------------------------------------------------------------------------
 #%% Define call functions
@@ -158,12 +203,12 @@ def build_posting_routes():
     post_url = lambda post_route: "".join(["/{camera_select:str}", post_route])
     post_data_routes = \
     [
-     Route(post_url("/bdb/metadata/camerainfo"), post_metadata("camerainfo"), methods=["POST"]),
-     Route(post_url("/bdb/metadata/snapshots"), post_metadata("snapshots"), methods=["POST"]),
-     Route(post_url("/bdb/metadata/objects"), post_metadata("objects"), methods=["POST"]),
-     Route(post_url("/bdb/metadata/backgrounds"), post_metadata("backgrounds"), methods=["POST"]),
-     Route(post_url("/bdb/image/snapshots/{epoch_ms:int}"), post_image("snapshots"), methods=["POST"]),
-     Route(post_url("/bdb/image/backgrounds/{epoch_ms:int}"), post_image("backgrounds"), methods=["POST"])
+     Route(post_url("/bdb/metadata/camerainfo"), post_camerainfo, methods=["POST"]),
+     Route(post_url("/bdb/metadata/backgrounds"), post_background_metadata, methods=["POST"]),
+     Route(post_url("/bdb/metadata/objects"), post_object_data, methods=["POST"]),
+     Route(post_url("/bdb/metadata/snapshots"), post_snapshot_metadata, methods=["POST"]),
+     Route(post_url("/bdb/image/backgrounds/{epoch_ms:int}"), post_background_image_data, methods=["POST"]),
+     Route(post_url("/bdb/image/snapshots/{epoch_ms:int}"), post_snapshot_image_data, methods=["POST"])
     ]
     
     return post_data_routes
